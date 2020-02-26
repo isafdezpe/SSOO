58e3e6ab8121feb3b3010788278e53e586d428da
isafdezpe <isafdezpe98@gmail.com>
2020-02-26 19:51:42 +0100

    ej0

--- V1/Processor.c
+++ V1/Processor.c
@@ -217,7 +217,19 @@ void Processor_DecodeAndExecuteInstruction() {
 			registerPSW_CPU=Processor_CopyFromSystemStack(MAINMEMORYSIZE-2);
 			break;		
 
-		
+		// Instruction MEMADD
+		case MEMADD_INST:
+			registerMAR_CPU=operand2;
+			// Send to the main memory controller the address in which the reading has to take place: use the address bus for this
+			Buses_write_AddressBus_From_To(CPU, MMU);
+			// Tell the main memory controller to read
+			registerCTRL_CPU=CTRLREAD;
+			Buses_write_ControlBus_From_To(CPU,MMU);
+			// Copy the read data to the accumulator register
+			registerAccumulator_CPU= operand1 + operand2;
+			Processor_CheckOverflow(operand1,operand2);
+			registerPC_CPU++;
+			break;
 
 		// Unknown instruction
 		default : 
